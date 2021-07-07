880507764d14c4b4ba6285caf07ae3ac403064f2
Phạm Minh Tân <tan.lk16.cla@gmail.com>
2021-07-07 12:49:00 +0700

    done sort

--- app/src/main/java/com/example/svbookmarket/activities/data/CommentRepository.kt
+++ app/src/main/java/com/example/svbookmarket/activities/data/CommentRepository.kt
@@ -4,6 +4,8 @@ import com.example.svbookmarket.activities.common.Constants
 import com.example.svbookmarket.activities.model.Comment
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.Query
+import java.time.Instant.now
+import java.util.*
 import javax.inject.Inject
 import javax.inject.Named
 import javax.inject.Singleton
@@ -13,10 +15,15 @@ class CommentRepository@Inject constructor(
     @Named(Constants.BOOK_REF) private val bookCollRef: CollectionReference
 ) {
     fun getAllCommentOfBook(bookId: String): Query{
-        return bookCollRef.document(bookId).collection("Comment")
+        return bookCollRef.document(bookId).collection("Comment").orderBy(
+            "time",
+            Query.Direction.DESCENDING
+        )
     }
     fun createNewComment(bookId: String, comment: String, userId: String){
+        val time = Date()
         val commentMap = hashMapOf(
+            "time" to time,
             "userId" to userId,
             "comment" to comment,
         )
